6b0423f1c4334895127dab946ad8433c790efb81
mikailaAkeredolu <mikaila@tech552.com>
2024-03-12 15:55:32 -0400

    created git repo locally

---
+++ src/main/java/sample/Playground.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Playground { //start of my class file
+    public static void main(String[] args) {
+
+            int age = 9;
+            if(age > 18){
+                System.out.println("You can party with us at YearUp");
+            } else if(age == 18){
+                System.out.println("Can also party with us");
+            }else if(age < 10){
+                System.out.println("Way too young to party with us");
+            } else{
+                System.out.println("Too young to party with us!!!!");
+            }
+
+    }
+
+} // end of my class file
